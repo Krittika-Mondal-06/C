a2d4ea119cce5abc961afb906a71916ad8cead79
Krittika-Mondal-06 <krittika.mon606@gmail.com>
2024-09-10 11:09:45 +0530

    Made Changes

---
+++ LabAssignment2/EvenorOddUsingfunctions.C
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int is_even(int num)
+{
+if (num % 2 == 0)
+{
+return 1;
+} else
+{
+return 0;
+}
+}
+int main()
+{
+int num;
+printf("Enter a number: ");
+scanf("%d", &num);
+if (is_even(num))
+{
+printf("%d is even.\n", num);
+} else
+{
+printf("%d is odd.\n", num);
+}
+return 0;
+}
