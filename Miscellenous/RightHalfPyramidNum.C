952b221246e553843b9485b1a989879ff03fdc88
Krittika-Mondal-06 <krittika.mon606@gmail.com>
2024-09-16 00:53:26 +0530

    Another Code

---
+++ Miscellenous/RightHalfPyramidNum.C
@@ -0,0 +1,18 @@
+// C program to print right half pyramid pattern of number
+#include <stdio.h>
+
+int main()
+{
+    int rows = 5;
+
+    
+    for (int i = 0; i < rows; i++) {
+
+        
+        for (int j = 0; j <= i; j++) {
+            printf("%d ", j + 1);
+        }
+        printf("\n");
+    }
+    return 0;
+}
