d7fbac7ed01ac4c9561c65ab537698f85b814323
Krittika-Mondal-06 <krittika.mon606@gmail.com>
2024-09-20 19:19:48 +0530

    Another Code

---
+++ LabAssignment3/AddandSubMatrices.C
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+
+    printf("Enter the number of rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+
+    int matrix1[rows][cols], matrix2[rows][cols], result[rows][cols];
+
+    printf("Enter elements of the first matrix:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &matrix1[i][j]);
+        }
+    }
+
+    printf("Enter elements of the second matrix:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &matrix2[i][j]);
+        }
+    }
+
+    // Addition
+    printf("Addition of matrices:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            result[i][j] = matrix1[i][j] + matrix2[i][j];
+            printf("%d ", result[i][j]);
+        }
+        printf("\n");
+    }
+
+    // Subtraction
+    printf("Subtraction of matrices:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            result[i][j] = matrix1[i][j] - matrix2[i][j];
+            printf("%d ", result[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
